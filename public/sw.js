e9130957543cf4f837645cb608dad19613b5375b
PedroDieb <joaopedrodieb1@gmail.com>
2026-08-26 15:29:24 -0300

    PWA: icones PNG (apple-touch-icon), manifest atualizado, sw v2

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'gymflow-v1';
-const APP_SHELL = ['./', 'manifest.webmanifest', 'icon.svg'];
+const CACHE_NAME = 'gymflow-v2';
+const APP_SHELL = ['./', 'manifest.webmanifest', 'icon.svg', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
